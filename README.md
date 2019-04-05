149a0091e7fbc4d53a9b210cbaf3fd4c20357ae3
Destiny <dshillis1@gmail.com>
2019-04-05 11:52:16 -0600

    Players Move

---
+++ README.md
@@ -0,0 +1,30 @@
+# Game Project
+
+I want to build an original Warcraft style game where you build up a city's defenses, guard against attacking enemies and wander the map 
+fighting enemies and gathering friends. It will be a basic grid layout where the player moves around the squares freely until they run into 
+an enemy horde. Then a battle ensues. Battle options will be pretty simple, archers shooting projectiles and knights fighting on the ground.
+Battles will start automatically upon entering a square occupied by an enemy. All attacks will have randomized accuracy and damage. Player 
+will have a home base to defend. If it is destroyed, the game ends. Ideally there will be 2+ computer enemy cities to fight and 1 city that 
+will ally with you and add to your attack force. Game is won once all enemies are destroyed.
+
+## Steps for MVP
+* Create a grid.
+* Create home city to defend. Defense set at 500. 
+* Create fighters and make them able to move around the board.
+* Create enemy city with enemies that defend their city.
+* Set up timer.
+* Have enemies pop up and randomly attack home city.
+* As time increases, so do enemy hordes.
+* Home city defense goes up as player levels up. 
+
+## Bonus Objectives
+* Create ally city that will add to player's attack and defense as long as player is near it.
+* Player needs to protect ally city + home city.
+* Player can build new buildings in their home city that will increase attack and/or defense.
+* Player can find random things that add to attack/defense around the board.
+* Fighters include knights that fight in close proximity and archers that can launch projectiles.
+* Enemy fighters also randomly wander or spawn anywhere on the board.
+* Create more enemy and ally cities.
+* Bigger map/side scroll
+
+![wireframe](images/game-project-wireframe.jpg)
---
+++ js/app.js
@@ -0,0 +1,141 @@
+// Create a grid.
+    // how big/number of squares? 
+    // size of squares?
+    // starting with 20 x 20, 30px x 30px squares
+    $('body').append('<div class="container"><div>')
+    for (let x = 0; x < 20; x++){
+        for (let y = 0; y < 20; y++){
+        $('.container').append(`<div class="grid-squares" x="${[x]}" y="${[y]}"></div>`)
+        }
+    }
+
+// Create home city to defend. Defense set at 500.
+    // how many squares does it take up? can you move it? 4 x 4 at 0,3
+    $('[x ="0"][y="3"]').append('<div class="home-city"></div>')
+    $('[x ="0"][y="4"]').append('<div class="home-city"></div>')
+    $('[x ="1"][y="3"]').append('<div class="home-city"></div>')
+    $('[x ="1"][y="4"]').append('<div class="home-city"></div>')
+
+const homeCity = {
+    defenses: 500,
+    location: ["0,3", "0,4", "1,3", "1,4"], // not sure this is the way I want to save this
+}
+
+
+// Create fighters and make them able to move around the board.
+    // how many to defend city?  start with 8
+    // how to get them to move?
+    // how to deterimine if they are player's people or enemies? class="home-knight"
+    // how to spawn new ones (if necessary)?
+    // how to attack? 
+        //randomize attacks (accuracy and damage inflicted)
+    // how to defend?
+
+
+class Knights {
+    constructor(hp, accuracy, damage){
+        this.hp = hp;
+        this.accuracy = accuracy;
+        this.damage = damage;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    attack(){
+
+    }
+}
+
+class KnightFactory {
+    constructor(){
+        this.knights = [];
+    }
+    generateKnight(){
+        const newKnight = new Knights(this.knights.length);
+    }
+    findKnight(index){
+        return this.knights[index];
+    }
+} 
+
+let knight1 = new Knights(100, 1, 15);
+$('[x ="0"][y="2"]').append('<div class="home-knight" id="1"></div>')
+$('[x ="1"][y="2"]').append('<div class="home-knight" id="1"></div>')
+
+
+
+// movement, figure out how to move them individually.
+// make it a function and call it in the class somehow?
+
+$(document).keydown(function(e){
+    if (e.keyCode === 37){ 
+        direction = 'left';
+        $('.home-knight').finish().animate({
+            left: '-=32'
+        });
+        console.log("left pressed");
+    } else if (e.keyCode === 38){
+        direction = 'up';
+        $('.home-knight').finish().animate({
+            top: '-=36'
+        });
+        console.log("up pressed");
+    } else if (e.keyCode === 39){
+        direction = 'right';
+        $('.home-knight').finish().animate({
+            left: '+=32'
+        });
+        console.log("right pressed");
+    } else if (e.keyCode === 40){
+        direction = 'down';
+        $('.home-knight').finish().animate({
+            top: '+=36'
+        });                
+        console.log("down pressed");
+    }
+});
+
+
+
+
+// $('.home-knight').keydown(function(){
+//     move();
+// })
+
+
+
+
+// Create enemy city with enemies that defend their city.
+    // how many to defend city? how to get them to move? 
+    // how to deterimine they are enemies?
+    // how to spawn next to home city?
+    // how to attack? 
+        //randomize attacks (accuracy and damage inflicted)
+    // how to defend?
+
+// Set up timer.
+    // set up in seconds
+    // what links to timer? 
+        // everyone's attacks (can only hit so often)
+        // enemy spawn times and amounts (randomized)
+        // level ups
+        // do cities build defenses back up? 
+        // should timer appear on screen?
+
+// Have enemies pop up and randomly attack home city.
+    // how to randomize their spawns
+    // randomize their attacks (accuracy and damage inflicted)
+    // give them hp stat, they disappear when it reaches 0
+
+// As time increases, so do enemy hordes.
+
+
+// BONUS
+// Create ally city that will add to player's attack and defense as long as player is 
+// near it.
+    // how does it know when the player is near the city?
+    // add defense/extra hp to home city
+    // add attack accuracy/damage inflicted to player's people?
+
+// Player needs to protect ally city + home city.
+
